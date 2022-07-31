dd6a1674ad2c99e96e902e38daa2f8c4ff61d290
dunnjacoba <101910318+dunnjacoba@users.noreply.github.com>
2022-07-31 02:52:42 -0500

    'Add navbar styling'

--- react/chaosreads/src/layout/NavBar.jsx
+++ react/chaosreads/src/layout/NavBar.jsx
@@ -6,7 +6,13 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 function NavBar() {
   return (
-    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Navbar
+      className="navbar"
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+    >
       <Container>
         <Navbar.Brand href="/">Chaos Reads Things</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
